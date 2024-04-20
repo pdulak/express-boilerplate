62a6623f13441843fac7c953dd3c7e2e00f78cd6
Pawel Dulak <pawel.dulak@gmail.com>
2024-04-20 18:21:54 +0200

    chore: removed unused file

--- middlewares/morganMiddleware.js
+++
@@ -1,9 +0,0 @@
-const morgan = require('morgan');
-const logger = require('../tools/logger');
-
-function morganLog(req, res, next) {
-    console.log(morgan('combined'));
-    next();
-}
-
-module.exports = { morganLog };
